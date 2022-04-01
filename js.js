1caea6838d6f62947a8da48da906e830576a3a27
EUCLIDES PEREZ <89555967+euclidesseg@users.noreply.github.com>
2022-03-31 20:52:26 -0500

    Update js.js

--- js.js
+++ js.js
@@ -9,18 +9,34 @@ function decirNombre(nombre){
         }
 }
 decirNombre(56);*/
-var n1 = prompt("ingrse el primer numero");
+
+
+/*var n1 = prompt("ingrse el primer numero");
 var n2 = prompt("ingrse el segundo numero ");
 var resultado
 
 function calcularNumeros(num1,num2){
     resultado = parseInt(num1 ) + parseInt(num2);
     return resultado
-    //alert(resultado)
+    //alert(resultado) 
     
 }
 var resulttado = calcularNumeros(n1,n2)
 function mostrarCalculo(){
     alert(resultado)
 }
-mostrarCalculo()
+mostrarCalculo()*/
+
+//objectos
+
+var objeto = {
+    nombre: "euclides",
+    apellido: "perez",
+    edad: 25,
+    direccion:{
+        barrrio: "porvenir",
+        calle:55,
+    },
+    hola: function hola (){alert("hola")},
+}
+console.log( objeto.nombre +""+ objeto.apellido +""+ objeto.edad);
