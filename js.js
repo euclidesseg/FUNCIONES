b630ba1d83155e1951484205d96a79419d3acdd9
EUCLIDES PEREZ <89555967+euclidesseg@users.noreply.github.com>
2022-03-31 19:52:24 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-//var nom = prompt("ingrese su nombre");
+/*/var nom = prompt("ingrese su nombre");
 function decirNombre(nombre){
     nombre || (nombre =  "User");
     alert("mi nombre es  "+nombre);
@@ -8,4 +8,19 @@ function decirNombre(nombre){
             alert("el nombre no es valido");
         }
 }
-decirNombre(56);
+decirNombre(56);*/
+var n1 = prompt("ingrse el primer numero");
+var n2 = prompt("ingrse el segundo numero ");
+var resultado
+
+function calcularNumeros(num1,num2){
+    resultado = parseInt(num1 ) + parseInt(num2);
+    return resultado
+    //alert(resultado)
+    
+}
+var resulttado = calcularNumeros(n1,n2)
+function mostrarCalculo(){
+    alert(resultado)
+}
+mostrarCalculo()
