b2c71be2bb57ad64dcf43411a8dc6cfea33a2d18
euclidesseg <euder0206@gmail.com>
2022-04-07 16:09:26 -0500

    Update javas4.js

---
+++ javas4.js
@@ -0,0 +1,7 @@
+//     var numero  = parseInt(prompt("ingrese el numero que desee elevar al cubo"));
+//     var cubo;
+//     function cubo(valor){
+//         cubo = Math.pow(valor,3);
+//         alert("el resultado de " + valor +" al cubo es " + cubo);
+// }
+// cubo(numero);
