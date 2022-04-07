d06a5c05230939f45feeceebdb187cdc70646b41
euclidesseg <euder0206@gmail.com>
2022-04-07 15:01:18 -0500

    Create javas2.js

---
+++ javas2.js
@@ -0,0 +1,10 @@
+// var km = parseInt(prompt("ingrese kilometros"));
+// var mts_h;
+// var millas_h;
+// function velocidad(kilometro){
+//     mts_h = kilometro * 1000;
+//     millas_h = kilometro/1609
+//     alert("el valor de kilometros pasados a metros es " + mts_h +"\n"
+//         +" el valor de kilometros pasados a millas es " + millas_h)
+// }
+// velocidad(km);
