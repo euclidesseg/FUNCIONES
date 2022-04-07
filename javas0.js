ae308c4e90531e6164ab667f7a56d6f7d0133cdd
euclidesseg <euder0206@gmail.com>
2022-04-07 16:09:30 -0500

    Create javas0.js

---
+++ javas0.js
@@ -0,0 +1,42 @@
+var nom = prompt("ingrese su nombre");
+function decirNombre(nombre){
+    nombre || (nombre =  "User");
+    alert("mi nombre es  "+nombre);
+        if(typeof nombre === "string"){
+            alert("su nombre es "+ nombre)
+        }else{
+            alert("el nombre no es valido");
+        }
+}
+decirNombre(56);
+
+
+/*var n1 = prompt("ingrse el primer numero");
+var n2 = prompt("ingrse el segundo numero ");
+var resultado
+
+function calcularNumeros(num1,num2){
+    resultado = parseInt(num1 ) + parseInt(num2);
+    return resultado
+    //alert(resultado) 
+    
+}
+var resulttado = calcularNumeros(n1,n2)
+function mostrarCalculo(){
+    alert(resultado)
+}
+mostrarCalculo()*/
+
+//objectos
+
+/*var objeto = {
+    nombre: "euclides",
+    apellido: "perez",
+    edad: 25,
+    direccion:{
+        barrrio: "porvenir",
+        calle:55,
+    },
+    hola: function hola (){alert("hola")},
+}
+console.log( objeto.nombre +""+ objeto.apellido +""+ objeto.edad);*/
