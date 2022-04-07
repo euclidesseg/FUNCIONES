17399088d7dbf5f1264fbd1cc5de452497bc79a0
euclidesseg <euder0206@gmail.com>
2022-04-07 16:30:34 -0500

    Update javas5.js

--- javas5.js
+++ javas5.js
@@ -1,10 +1,11 @@
 var nom = prompt("ingrese su nombre");
 function decirNombre(nombre){
 
-        if(typeof nombre !=== "string"){
-            alert("el nombre no es valido por favor ingrese el correcto");
-        }else{
-            alert("su nombre es " + nombre);
+   
+        if(typeof nombre === "string"){
+            alert("su nombre es "+ nombre)
+        }else if(typeof nombre != "string"){
+            alert("el nombre no es valido por favor ingrese uno valido");
         }
 }
 decirNombre(nom);
